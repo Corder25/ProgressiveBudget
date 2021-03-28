a340ef4a281cd9483a947f508ab48c3c9d3ba746
Cam corder <corder2425@yahoo.com>
2021-03-28 12:08:52 -0500

    fixed service worker

---
+++ ProgressiveBudget/README.md
@@ -0,0 +1,3 @@
+## ProgressiveBudget
+
+![Screenshot (65)](https://user-images.githubusercontent.com/69650837/112408240-74ce2b00-8ce5-11eb-9009-b4948f49d1fd.png)
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 const app = express();
 
@@ -15,13 +16,24 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
-
 // routes
 app.use(require("./routes/api.js"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb+srv://dbCorder:Lakers2424@cluster0.lly1a.mongodb.net/ProgressiveBudget?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App running on port ${PORT}!`);
+    });
+})
+
+
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
