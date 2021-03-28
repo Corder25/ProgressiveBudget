3876461100111dffb0380625581ea2d5b2a65250
Cam corder <corder2425@yahoo.com>
2021-03-28 12:26:11 -0500

    fixed git hub issue, everything working

--- ProgressiveBudget/README.md
+++
@@ -1,3 +0,0 @@
-## ProgressiveBudget
-
-![Screenshot (65)](https://user-images.githubusercontent.com/69650837/112408240-74ce2b00-8ce5-11eb-9009-b4948f49d1fd.png)
--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = 3000;
 
 const app = express();
 
@@ -15,26 +14,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
 // routes
 app.use(require("./routes/api.js"));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb+srv://dbCorder:Lakers2424@cluster0.lly1a.mongodb.net/ProgressiveBudget?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }
+  process.env.MONGODB_URI || 'mongodb+srv://dbCorder:Lakers2424@cluster0.7p4nf.mongodb.net/budget?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+  }
 ).then(() => {
-    app.listen(PORT, () => {
-        console.log(`App running on port ${PORT}!`);
-    });
+  app.listen(PORT, () => {
+      console.log(`App running on port ${PORT}!`);
+  });
 })
 
 
 
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+ 
